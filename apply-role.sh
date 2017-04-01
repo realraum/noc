bd518a45d608130161a0ea323a947489789c4996
Christian Pointner <equinox@realraum.at>
2017-04-01 03:17:52 +0200

    added base role

---
+++ apply-role.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "$1" ] || [ -z "$2" ] ; then
+  echo "$0 <host(s)> <role>"
+  exit 1
+fi
+hosts="$1"
+shift
+role="$1"
+shift
+
+echo "######## applying the role '$role' to host(s) '$hosts' ########"
+exec ansible-playbook -e "hosts=$hosts" -e "role=$role" $@ generic.yaml
