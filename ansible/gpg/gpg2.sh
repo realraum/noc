a185b58dc62dfc9e613592774d566b44de54715f
Christian Pointner <equinox@realraum.at>
2018-04-21 22:09:32 +0200

    ansible: set trust-model to always for vault handling scripts

--- ansible/gpg/gpg2.sh
+++ ansible/gpg/gpg2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec gpg2 --keyring "${BASH_SOURCE%/*}/vault-keyring.gpg" --secret-keyring /dev/null --no-default-keyring $@
+exec gpg2 --keyring "${BASH_SOURCE%/*}/vault-keyring.gpg" --secret-keyring /dev/null --no-default-keyring --trust-model always $@
