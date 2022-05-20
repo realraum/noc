375be95166bd3d9131031d3547d5bcafc541f78d
Christian Pointner <equinox@realraum.at>
2022-05-20 14:21:07 +0200

    gpg vs gpg2

--- ansible/gpg/get-vault-pass.sh
+++ ansible/gpg/get-vault-pass.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-gpg2 --decrypt --batch < "${BASH_SOURCE%/*}/vault-pass.gpg" 2> /dev/null
+
+BIN=$(which gpg2)
+if [ $? -ne 0 ]; then
+  BIN=$(which gpg)
+fi
+
+$BIN --decrypt --batch < "${BASH_SOURCE%/*}/vault-pass.gpg" 2> /dev/null
--- ansible/gpg/gpg2.sh
+++ ansible/gpg/gpg2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-exec gpg2 --keyring "${BASH_SOURCE%/*}/vault-keyring.gpg" --secret-keyring /dev/null --no-options --no-default-keyring --trust-model always --keyid-format 0xlong $@
+
+BIN=$(which gpg2)
+if [ $? -ne 0 ]; then
+  BIN=$(which gpg)
+fi
+
+exec $BIN --keyring "${BASH_SOURCE%/*}/vault-keyring.gpg" --secret-keyring /dev/null --no-options --no-default-keyring --trust-model always --keyid-format 0xlong $@
