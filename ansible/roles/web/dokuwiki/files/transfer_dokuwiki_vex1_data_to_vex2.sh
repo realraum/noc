2599f60c37da5ce1ca51e2ffa8f2657969bd8c5a
Bernhard Tittelbach <bernhard@tittelbach.org>
2025-11-09 10:35:42 +0100

    vex2: script .. mkdir log in case of empty data dir

--- ansible/roles/web/dokuwiki/files/transfer_dokuwiki_vex1_data_to_vex2.sh
+++ ansible/roles/web/dokuwiki/files/transfer_dokuwiki_vex1_data_to_vex2.sh
@@ -4,6 +4,9 @@ ssh-add ~/.ssh/id-rsa
 echo ""
 echo "=========== Rsyncing Dokuwiki Data from vex  ======================================"
 rsync -e 'ssh -o HostKeyAlgorithms=+ssh-rsa -o PubkeyAcceptedKeyTypes=+ssh-rsa -o KexAlgorithms=curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group-exchange-sha1,diffie-hellman-group14-sha1,diffie-hellman-group1-sha1' -v -r --size-only --chown dokuwiki:dokuwiki vex.realraum.at:/var/www/dokuwiki/data/ /srv/dokuwiki/data/
+mkdir -p /srv/dokuwiki/data/log/error
+chown dokuwiki:dokuwiki -R /srv/dokuwiki/data/log
+
 echo ""
 echo "=========== Rsyncing Dokuwiki ACL+Users from vex  ======================================"
 rsync -e 'ssh -o HostKeyAlgorithms=+ssh-rsa -o PubkeyAcceptedKeyTypes=+ssh-rsa -o KexAlgorithms=curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group-exchange-sha1,diffie-hellman-group14-sha1,diffie-hellman-group1-sha1' -v -r --size-only --chown dokuwiki:dokuwiki vex.realraum.at:/var/www/dokuwiki/conf/acl.auth.php vex.realraum.at:/var/www/dokuwiki/conf/users.auth.php /srv/dokuwiki/acl/
